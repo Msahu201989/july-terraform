cece79deb91973de51c7eb78f3449d5d45a13ee1
Mukesh <Stunty@007>
2023-07-20 11:50:46 +0530

    learning devops

--- 04-remote-state/ec2.tf
+++ 04-remote-state/ec2.tf
@@ -4,8 +4,8 @@ resource "aws_instance" "sample" {
   vpc_security_group_ids = [aws_security_group.allow_sssh.id]
 
   tags = {
-    Name = "Hello"
-
+    Name = upper(var.tagName)
   }
 }
 
+variable "tagName" {}
